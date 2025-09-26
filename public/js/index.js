e72945137799f45d47c3266470322eb7064bdeb3
Matthew.Avrillio <matthew.avrillio@berca.co.id>
2025-09-26 14:17:55 +0700

    Added Loading Animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loaderWrapper = document.getElementById("loader-wrapper");
+
+  setTimeout(() => {
+    // Add a class to hide the loader
+    loaderWrapper.classList.add("hidden");
+  }, 3000);
+
+  // Optionally, remove the loader from the DOM after the animation finishes
+  loaderWrapper.addEventListener("transitionend", function () {
+    loaderWrapper.remove();
+  });
+});
+
 function animateDots(elementId, maxDots, interval) {
   const element = document.getElementById(elementId);
   let dotCount = 0;
@@ -5,6 +19,8 @@ function animateDots(elementId, maxDots, interval) {
   setInterval(() => {
     dotCount = (dotCount % maxDots) + 1; // Cycle through 1 to maxDots
     element.textContent = ":now entering" + ".".repeat(dotCount);
+    if (dotCount == 3) dotCount = 0;
+    // console.log(dotCount);
   }, interval);
 }
 
