96e516f9fb1f262507b1e75051afe2cdbc0448e0
mattsmatt <matthew.avrillio@binus.ac.id>
2025-09-28 01:48:25 +0700

    removed  finishloadingsound code

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,4 @@
 var hoverSound;
-var finishLoadingSound;
 
 function init() {
   const loaderWrapper = document.getElementById("loader-wrapper");
@@ -10,14 +9,10 @@ function init() {
   hoverSound = new Audio(
     "https://github.com/mattsmatt/mattsmatt.neocities.org/raw/refs/heads/main/public/assets/audio/onClick.mp3"
   );
-  finishLoadingSound = new Audio(
-    "https://github.com/mattsmatt/mattsmatt.neocities.org/raw/refs/heads/main/public/assets/audio/onFinishLoading.mp3"
-  );
 
   setTimeout(() => {
     // Add a class to hide the loader
     loaderWrapper.classList.add("hidden");
-    finishLoadingSound.play();
   }, 3000);
 
   // // Optionally, remove the loader from the DOM after the animation finishes
