b5d6703dea14a246cd621f6714ed27fef4e13596
mattsmatt <matthew.avrillio@binus.ac.id>
2025-09-28 00:49:31 +0700

    added home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,25 +1,8 @@
-const hoverArea = document.getElementById("hoverArea");
-var hoverSound;
-
-function animateDots(elementId, maxDots, interval) {
-  const element = document.getElementById(elementId);
-  let dotCount = 0;
-
-  setInterval(() => {
-    dotCount = (dotCount % maxDots) + 1; // Cycle through 1 to maxDots
-    element.textContent = ":now entering" + ".".repeat(dotCount);
-    if (dotCount == 3) dotCount = 0;
-    // console.log(dotCount);
-  }, interval);
-}
-
-function init() {
+function showLoading() {
   const loaderWrapper = document.getElementById("loader-wrapper");
   if (loaderWrapper.classList.contains("hidden")) {
     loaderWrapper.classList.remove("hidden");
   }
-
-  hoverSound = new Audio("./assets/audio/onMouseEnter.mp3");
 
   setTimeout(() => {
     // Add a class to hide the loader
@@ -35,8 +18,6 @@ function init() {
 function redirect(destination) {
   const loaderWrapper = document.getElementById("loader-wrapper");
 
-  hoverSound.play();
-
   if (loaderWrapper.classList.contains("hidden")) {
     loaderWrapper.classList.remove("hidden");
   }
@@ -46,6 +27,4 @@ function redirect(destination) {
   }, 3000);
 }
 
-document.addEventListener("DOMContentLoaded", init());
-
-animateDots("loadingText", 3, 500);
+document.addEventListener("DOMContentLoaded", showLoading());
